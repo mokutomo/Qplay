ed9862f1c976f6426c669c7f5ea957bc3ace4bcf
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-03-21 10:29:41 +0000

    削除機能の追加

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -9,26 +9,31 @@ class ContentsController < ApplicationController
 		content.save
 		if params["a"]
 			redirect_to new_blog_content_path
-	    else
+		else
 			redirect_to root_path
-	    end
+		end
 	end
 	def index
 		@content = Content.where(blog_id: params[:blog_id])
 	end
 	def edit
-	@blog = Blog.find(params[:blog_id])
-	@content = Content.find(params[:id])
-    end
-    def update
-	content = Content.find(params[:id])
-	content.update(content_params)
-	redirect_to blog_contents_path
-    end
+		@blog = Blog.find(params[:blog_id])
+		@content = Content.find(params[:id])
+	end
+	def update
+		content = Content.find(params[:id])
+		content.update(content_params)
+		redirect_to blog_contents_path
+	end
+	def destroy
+		content = Content.find(params[:id])
+		content.destroy
+		redirect_to blog_contents_path
+	end
 
 	private
-    def content_params
-        params.require(:content).permit(:content_title, :url, :image, :body, :blog_id)
-    end
+	def content_params
+		params.require(:content).permit(:content_title, :url, :image, :body, :blog_id)
+	end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,18 @@
 class UsersController < ApplicationController
 	def show
-		@user = User.find(params[:id])
+		@user = User.find(current_user[:id])
 	end
 	def index
 	end
-
-
-
-
-
-
-
-
-
-
+	def destroy
+		blog = Blog.find(params[:id])
+		blog.destroy
+		redirect_to user_path
+	end
 
 	private
-    def blog_params
-        params.require(:blog).permit(:title, :location, :image)
-    end
+	def blog_params
+		params.require(:blog).permit(:title, :location, :image)
+	end
 end
 
