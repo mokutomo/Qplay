4c7a4860a3bc72fe44cc0bb26328e495e91ef8a6
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-03-23 11:10:01 +0000

    検索機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :blogs do
-resources :contents
-end
 resources :users
 resources :random_images
+
+resources :blogs do
+resource :favorites, only: [:create, :destroy]
+resources :contents
+end
+
 
 
 root :to => 'blogs#index'
