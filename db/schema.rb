c6c373e48dde58993f15f7ab871f53831aab0ee6
Tomohiro Mokudai <mototomo3204@gmail.com>
2019-03-27 09:56:39 +0000

    ユーザーの削除機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_060955) do
+ActiveRecord::Schema.define(version: 2019_03_27_042122) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
